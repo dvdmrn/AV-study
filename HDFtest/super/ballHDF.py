6c1b96b03e88dd3e3aa29172b3a41fab18ded784
dvdmrn <dgm.marino@gmail.com>
2017-01-30 13:11:09 -0800

    readme updates

--- HDFtest/super/ballHDF.py
+++ HDFtest/super/ballHDF.py
@@ -17,6 +17,7 @@ import pylab
 from tables import *
 
 import sys
+import time
 
 HDFfilepath = sys.argv[1]
 soundFilePath = sys.argv[2]
@@ -30,6 +31,8 @@ class Animation(IsDescription):
 
 #opens h5file
 h5file = open_file(HDFfilepath, mode = "r", title = "Test file")
+
+
 table = h5file.root.animation
 #constructs array of frame vals
 frames = [x for x in table.iterrows()]
@@ -75,9 +78,9 @@ scaleFactor = 100
 
 
 #Load audio
-pygame.mixer.music.load(soundFilePath)
+audio = pygame.mixer.Sound(soundFilePath)
 
-pygame.mixer.music.play(-1, 0.0)
+audio.play(-1, 0)
 #Draw a circle
     #   unfortunately if you want antialiasing support is only available for
     #   the outline of a circle, so we must draw the filled circle beneath the outlined
@@ -93,22 +96,26 @@ def inferFrameRate(frames,soundFilePath):
 	return (len(frames)/a.get_length())
 	
 
-framerate = inferFrameRate(frames,soundFilePath)
-
-print("did inferframerate work? ", framerate)
+#framerate
+target_fps = (inferFrameRate(frames,soundFilePath))/2
+prev_time = time.time()
+title = "Compiled animation"
 
 #----------------------------------------------------------------
 #      pygame main loop
 #----------------------------------------------------------------
 
 while not done:
- 
-    # This sets the framerate
-    # Leave this out and we will use all CPU we can.
-    clock.tick(framerate)
     
-
-    framePosition = int(round(frames[frameIndex]*scaleFactor))
+    #-----------------------------------------------------------
+    # translates HDF to motion
+    #-----------------------------------------------------------
+    try:
+	    framePosition = int(round(frames[frameIndex]*scaleFactor))
+    except IndexError:
+    	print('finished iterating frames')
+    	done = True
+    	framePosition = 0
     xPos += 1
     yPos = (height/2) - framePosition
     if yPos < 0:
@@ -119,13 +126,30 @@ while not done:
         if event.type == pygame.QUIT: # If user clicked close
             done=True # Flag that we are done so we exit this loop
  
-     
+    #-----------------------------------------------------------
+    # rendering
+    #-----------------------------------------------------------
+
     # Clear the screen and set the screen background
     screen.fill(WHITE)
  
     circle((width/2),yPos,40,ballColor)
  
     frameIndex+=1
+    
+    #-----------------------------------------------------------
+    # framerate stuff
+    #-----------------------------------------------------------
+
+    #Timing code at the END!
+    curr_time = time.time()#so now we have time after processing
+    diff = curr_time - prev_time#frame took this much time to process and render
+    delay = max(1.0/target_fps - diff, 0)#if we finished early, wait the remaining time to desired fps, else wait 0 ms!
+    print('delay: ',delay)
+    time.sleep(delay)
+    fps = 1.0/(delay + diff)#fps is based on total time ("processing" diff time + "wasted" delay time)
+    prev_time = curr_time
+    pygame.display.set_caption("{0}: {1:.2f}".format(title, fps))
     # Updates screen: this MUST happen after all the other drawing commands.
     pygame.display.flip()
  
---
+++ viapoint_editor/README.md
@@ -0,0 +1,1 @@
+# viapoint editor
