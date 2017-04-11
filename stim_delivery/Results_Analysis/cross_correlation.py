9234f2b8799ed7bb6588231ef072eacf406b70ad
Alice Zhu <alice-desu@hotmail.com>
2017-04-11 15:28:04 -0700

    Added pearson and numpy correlation coefficient calculations

--- stim_delivery/Results_Analysis/cross_correlation.py
+++ stim_delivery/Results_Analysis/cross_correlation.py
@@ -22,6 +22,12 @@ from numpy import array,linspace,argmax
 from scipy.signal import correlate
 
 import matplotlib.pyplot as plt
+
+
+
+from scipy.stats.stats import pearsonr
+import numpy as np
+
     
 ########################################################################        
 
@@ -47,9 +53,21 @@ sr2,dt2=sample_rate_check(a2,b2,num2,sr2,dt2)
 a1=array(a1)
 b1=array(b1)
 
+#print ("a1 time: ", a1)
+#print ('/n')
+
+
+#print ("b1 keylogs: ", b1)
+#print ('/n')
+
 a2=array(a2)
 b2=array(b2)       
 
+#print ("a2 time: ", a2)
+#print ('/n')
+
+#print ("b2 frames: ", b2)
+#print ('/n')
 
 if((dt1-dt2)/dt2 < 0.001):
 
@@ -60,7 +78,30 @@ if((dt1-dt2)/dt2 < 0.001):
     if(num1>num2):
         a1=a1[0:num2]        
         b1=b1[0:num2]
-        
+
+
+    #TODO Coefficient Calculation
+
+    # Calculates a Pearson correlation coefficient and the p-value for testingnon-correlation.
+    # Only calculates  The Pearson correlation coefficient measures the linear relationshipbetween two datasets.
+    # Strictly speaking, Pearson's correlation requiresthat each dataset be normally distributed.
+    #  Like other correlationcoefficients, this one varies between -1 and +1 with 0 implying nocorrelation.
+
+    print("Pearson Linear Correlation and P-value")
+    #print(pearsonr([1, 2, 3], [5, 8, 10]))
+    print(pearsonr(b1, b2))
+    print('\n')
+
+    #numpys version of coefficient
+    list1 = [1, 2, 3]
+    list2 = [3, 5, 7]
+
+    print("Numpy Correlate Coefficient: ")
+    #np.corrcoef(list1, list2)[0, 1]
+    print(np.corrcoef(b1, b2)[0, 1])
+
+
+    # information for plots
 
     cc = correlate(b1,b2)
 
@@ -88,8 +129,11 @@ if((dt1-dt2)/dt2 < 0.001):
     
     time_history_plot(d,cc,3,'Delay(sec)','Rxy',title_string,'cross_correlation')
 
-    plt.show()        
-    
+    plt.show()
+
+
+
+
 else:
     
     print (" ")
